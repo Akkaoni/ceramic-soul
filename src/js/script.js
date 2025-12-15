63ffcbdc65d812c22456da733a8bc421d05bb830
Artur <akkaoniko@gmail.com>
2025-12-16 02:30:30 +0600

    a mobile version of the section has been created

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,29 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-});
+try {
+    new Swiper('.works__slider', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                spaceBetween: 35,
+            }
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { }
+
