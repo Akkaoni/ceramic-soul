ea19c9f65cf0bcbe06c1460d71049a5b9f144867
Artur <akkaoniko@gmail.com>
2025-12-16 00:31:30 +0600

    integrate swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
-
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+});
